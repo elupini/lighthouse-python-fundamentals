1de94eaa8b7ce9f30fa8e30648ff92b4a5f5a1c3
Elliot Lupini <elupini@me.com>
2020-08-17 13:31:27 -0700

    Numbers Tutorial Examples

---
+++ main.py
@@ -0,0 +1,25 @@
+anyNum = int(input("Please enter an integer "))
+
+tripleUp = anyNum
+quarterValue = anyNum
+
+anyNum -= 5
+print(anyNum)
+
+print('\n')
+
+tripleUp *= 3
+print(tripleUp)
+
+print('\n')
+
+quarterValue /= 4
+print(quarterValue)
+
+sum = 0
+
+for i in range (3):
+    num = int(input("Please enter an integer "))
+    sum += num
+    
+print(sum)
