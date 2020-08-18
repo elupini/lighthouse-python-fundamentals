3e116d5b1ab3f2042bb7e8b13c95e58a83dabb3a
Elliot Lupini <elupini@me.com>
2020-08-18 08:43:36 -0700

    Added num, string, and list examples

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
-anyNum = int(input("Please enter an integer "))
+#anyNum = int(input("Please enter an integer "))
 
-tripleUp = anyNum
-quarterValue = anyNum
+# tripleUp = anyNum
+# quarterValue = anyNum
 
-anyNum -= 5
-print(anyNum)
+# anyNum -= 5
+# print(anyNum)
 
-print('\n')
+# print('\n')
 
-tripleUp *= 3
-print(tripleUp)
+# tripleUp *= 3
+# print(tripleUp)
 
-print('\n')
+# print('\n')
 
-quarterValue /= 4
-print(quarterValue)
+# quarterValue /= 4
+# print(quarterValue)
 
-sum = 0
+# sum = 0
 
-for i in range (3):
-    num = int(input("Please enter an integer "))
-    sum += num
+# for i in range (3):
+#     num = int(input("Please enter an integer "))
+#     sum += num
     
-print(sum)
+# print(sum)
+
+# print("The square of {} is {}".format(anyNum, anyNum ** 2))
+
+# print("{} modulo 3 is {}".format(anyNum, anyNum % 3))
+
+# if (anyNum % 2) == 0:
+#     print("{} is divisible by 2".format(anyNum))
+    
+    
+# celestialObjects = [['Mercury', 'Venus', 'Earth', 'Mars'], ['Polaris', 'Arcturus', 'Spika', 'Alkaid'], ['Milky Way', 'Andromeda'], ['Opportunity', 'Curiosity', 'Perseverance']]
+
+# print(celestialObjects[0])
+# print(celestialObjects[1])
+# print(celestialObjects[2])
+# print(celestialObjects[3])
+
+# print(celestialObjects[1][2])
+
+# print(celestialObjects[3][::-1])
+
+
+randomObjects = []
+
+numOfStrings = int(input("How many strings would like to enter? "))
+
+for i in range(numOfStrings):
+    userInput = input("Please enter a string: ")
+    randomObjects.append(userInput)
+    
+print(randomObjects)
+
+letter = input("Which letter would you like to count? ")
+letterCounter = 0
+
+for i in range(numOfStrings):
+    letterCounter += randomObjects[i].count(letter)
+    
+print("The number of occurrences of the letter '{}' is {}".format(letter, letterCounter))
